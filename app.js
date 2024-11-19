05479dd3df7a294f8ec59f6201473509da1e0946
Virginia Dooley <vcdooley@gmail.com>
2024-11-19 19:06:53 +0000

    Add public HTML & get-poem endpoint

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import chatCompletion from './services/openai.js';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get(
-    '/', 
-    (req, res) => {
-        res.send('Hello from my lovely new server!');
+    '/get-poem',
+    async (req, res) => {
+        const response = await chatCompletion(
+            "Write a short, expressive poem about lemons, as if you are hungry. Just four lines!"
+        );
+        res.send({ poem: response });
+    }
+);
+
+app.get(
+    '/test',
+    async (req, res) => {
+        const response = await chatCompletion("Provide an amusing sentence.");
+        res.send(response);
     }
 );
 
